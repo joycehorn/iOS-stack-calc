3327cdb4afe1820b168ada4841d22cf9ff5533c1
Joyce Horn <joycehorn@joyces-air.localdomain>
2018-06-11 22:33:58 -0300

    Varsion 1.0

---
+++ Auto Layout Stack Calc/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Auto Layout Stack Calc
+//
+//  Created by Joyce Horn on 6/11/18.
+//  Copyright © 2018 Horn Fonteles Tech. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
